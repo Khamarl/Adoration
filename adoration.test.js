f92971f3e4d8f5861fa4606a6e25bd6e157b5134
Khamarl C <khamarlchristie@hotmail.co.uk>
2022-09-15 10:36:37 +0100

    add each test

--- adoration.test.js
+++ adoration.test.js
@@ -1,3 +1,5 @@
+const each = require('jest-each').default;
+
 const { showHowMuchILoveYou } = require("./adoration");
 
 describe("showHowMuchILoveYou", () => {
@@ -7,9 +9,11 @@ describe("showHowMuchILoveYou", () => {
     it("Is a function ", () => {
         expect(showHowMuchILoveYou instanceof Function).toEqual(true)
     });
-    it("returns a string with correct number of i", () => {
-        expect(showHowMuchILoveYou(5)).toBe("I love you thiiiiis much")
-    });
+    each([["I love you thiiiiis much", 5],
+        ["I love you thiiis much", 3]]).test("Returns %s when passed %s", (expected, amount) => {
+            expect(showHowMuchILoveYou(amount)).toBe(expected);
+        })
+
     it("Throws an error passed a string", () => {
         expect(() => {
             showHowMuchILoveYou('with all my heart')
